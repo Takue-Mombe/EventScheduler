f07c81f1046bd817aca62c94f46e4a008ef9282d
mombe <h220462g@hit.ac.zw>
2024-03-07 20:19:26 +0200

    forth Commit

--- Event-Scheduler/src/main/java/com/EventScheduler/Event/Scheduler/Configuration/UserConfig.java
+++ Event-Scheduler/src/main/java/com/EventScheduler/Event/Scheduler/Configuration/UserConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class UserConfig {
@@ -26,19 +30,20 @@ public class UserConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Configuration
-    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, "/users/register").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .formLogin().and()
-                    .httpBasic().and()
-                    .csrf().disable();
-        }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(HttpMethod.POST, "/users/register").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection
+
+        return http.build();
     }
 }
